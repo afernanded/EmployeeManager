1e3ed470f22ba35b2ba5e8e58da72eeed834729f
Alexander <alexanderfernandez161616@gmail.com>
2021-07-29 21:08:23 +0200

    [MOD] Service Class - create a few methods

--- src/main/java/com/afernanded/employeemanager/service/EmployeeService.java
+++ src/main/java/com/afernanded/employeemanager/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.afernanded.employeemanager.service;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,21 @@ public class EmployeeService {
 	public Employee addEmployee(Employee employee) {
 		employee.setEmployeeCode(UUID.randomUUID().toString());
 		return employeeRepo.save(employee);
+	}
+	
+	public List<Employee> findAllEmployees() {
+		return employeeRepo.findAll();
+	}
+	
+	public Employee findEmployeeById(Long id) {
+		return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException ("User by ID: " + id + " was not found."));
+	}
+	
+	public Employee updaEmployee(Employee employee) {
+		return employeeRepo.save(employee);
+	}
+	
+	public void deleteEmployee(Long id) {
+		employeeRepo.deleteEmployeeById(id);
 	}
 }
